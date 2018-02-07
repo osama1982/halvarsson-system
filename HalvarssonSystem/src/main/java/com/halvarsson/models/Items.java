e30b11c6d4d9ca539578c2834062eb665684c2a1
PC2 <PC2@DESKTOP-FD21ADS.LEXICON.local>
2018-02-07 13:43:01 +0100

    General Edits

--- HalvarssonSystem/src/main/java/com/halvarsson/models/Items.java
+++ HalvarssonSystem/src/main/java/com/halvarsson/models/Items.java
@@ -12,38 +12,46 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Entity
-@Table(name="maincourses")
+@Table(name="items")
 public class Items {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	@Column(name="mc_id")
+	@Column(name="itemId")
 	private int id;
 	
 	@NotNull
 	@Size(min = 3, max= 30, message = "Must be 3 characters at least")
-	@Column(name="mc_name")
+	@Column(name="itemName")
 	private String name;
 	
-	@Column(name="mc_desc")
+	@Column(name="itemDesc")
 	private String desc;
 	
 	
 	@NotNull
 	@Min(value = 0, message = "Price cannont be lower than zero")
-	@Column(name="mc_price")
+	@Column(name="itemPrice")
 	private double price;
 	
+	
+	//@Column(name="category_id") 
+	/*org.springframework.beans.factory.BeanCreationException: 
+	Error creating bean with name 'entityManagerFactory' 
+	defined in class path resource 
+	[org/springframework/boot/autoconfigure/orm/jpa/HibernateJpaAutoConfiguration.class]: 
+	Invocation of init method failed; nested exception is org.hibernate.AnnotationException: 
+	@Column(s) not allowed on a @ManyToOne property: com.halvarsson.models.Items.category*/
 	@ManyToOne
-	@Column(name="category_id")
 	private Categories category;
 	
 	public Items() {}
 	
-	public Items(String name, String desc, ItemSizes itemSize, double price) {
+	public Items(String name, String desc, ItemSizes itemSize, double price,Categories category) {
 		this.name = name;
 		this.desc = desc;
 		this.price = price;
+		this.category = category;
 		//this.itemSize = itemSize;
 	}
 	
