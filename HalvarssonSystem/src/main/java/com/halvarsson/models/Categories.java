f57786e93cd7c34c7ca76b81ff6b9081df7b70a5
PC2 <PC2@DESKTOP-FD21ADS.LEXICON.local>
2018-02-07 14:39:43 +0100

    edits

--- HalvarssonSystem/src/main/java/com/halvarsson/models/Categories.java
+++ HalvarssonSystem/src/main/java/com/halvarsson/models/Categories.java
@@ -3,14 +3,12 @@
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
-import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
 @Entity
@@ -18,7 +16,7 @@ import javax.validation.constraints.NotNull;
 public class Categories {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue //(strategy = GenerationType.AUTO)
 	//@Column(name="catId")
 	private int catId;
 	
--- HalvarssonSystem/src/main/java/com/halvarsson/models/Items.java
+++ HalvarssonSystem/src/main/java/com/halvarsson/models/Items.java
@@ -1,12 +1,10 @@
 package com.halvarsson.models;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import javax.persistence.Table;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -16,7 +14,7 @@ import javax.validation.constraints.Size;
 public class Items {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue
 	//@Column(name="itemId")
 	private int id;
 	
